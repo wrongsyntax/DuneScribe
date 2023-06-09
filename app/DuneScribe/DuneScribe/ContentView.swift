0e733075a8969d635f933ced40148f26c7236457
Uzair Tariq <uzair0213@icloud.com>
2023-06-08 23:52:31 -0600

    Created Xcode Project
    Initial creation of Xcode project, with no changes made at this point.

---
+++ app/DuneScribe/DuneScribe/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DuneScribe
+//
+//  Created by Uzair Tariq on 2023-06-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app/DuneScribe/DuneScribe/DuneScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuneScribeApp.swift
+//  DuneScribe
+//
+//  Created by Uzair Tariq on 2023-06-08.
+//
+
+import SwiftUI
+
+@main
+struct DuneScribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
